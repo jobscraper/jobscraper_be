a171143dd83ce091ca391bd5fe277748b8fe26a3
Kim G <xiaole.guo@yahoo.com>
2022-06-24 19:45:48 -0600

    add flexjob poro

---
+++ app/poros/flexjob.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# indeed job poro
+class Flexjob
+  def initialize(data)
+    @data = data
+  end
+
+  def title
+    @data.attributes['data-title'].value
+  end
+
+  def datePosted
+    @data.css('div.job-age')[0].children.last.text.strip
+  end
+
+  def url
+    'https://www.flexjobs.com/'+ @data.css('a.job-link')[0].attributes['href'].value
+  end
+
+  def description
+    @data.css('div.job-description')[0].children[0].text.strip
+  end
+
+  def location
+    @data.css('div.job-locations')[0].children.first.text.strip +
+    @data.css('span.job-tag')[0].children[0].text
+  end
+end
